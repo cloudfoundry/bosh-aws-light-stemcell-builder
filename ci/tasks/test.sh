43727400d3c353821c09e0873372acf58690f140
Lyle Franklin <lfranklin@pivotal.io>
2016-02-08 08:15:46 -0800

    Enable test.sh to be run locally
    - Doesn't make assumptions about folder names or executable locations
    
    [#113016609](https://www.pivotaltracker.com/story/show/113016609)

--- ci/tasks/test.sh
+++ ci/tasks/test.sh
@@ -2,13 +2,18 @@
 
 set -e
 
-source builder-src/ci/tasks/utils.sh
+release_dir="$( cd $(dirname $0)/../.. && pwd )"
 
-check_param access_key
-check_param secret_key
-check_param bucket_name
-check_param region
-check_param ami_fixture_id
+source ${release_dir}/ci/tasks/utils.sh
+
+tmp_dir="$(mktemp -d /tmp/stemcell_builder.XXXXXXX)"
+trap '{ rm -rf ${tmpdir}; }' EXIT
+
+: ${access_key:?}
+: ${secret_key:?}
+: ${bucket_name:?}
+: ${region:?}
+: ${ami_fixture_id:?}
 
 export AWS_ACCESS_KEY_ID=$access_key
 export AWS_SECRET_ACCESS_KEY=$secret_key
@@ -19,19 +24,28 @@ export AMI_FIXTURE_ID=$ami_fixture_id
 export OUTPUT_STEMCELL_PATH=$PWD
 
 echo "Checking Java configuration"
-$JAVA_HOME/bin/java -version
+if hash java 2>/dev/null; then
+  JAVA_EXEC="$(which java)"
+else
+  JAVA_EXEC="$JAVA_HOME/bin/java"
+fi
+${JAVA_EXEC} -version
 
 echo "Checking EC2 CLI has been properly installed"
-which ec2-describe-regions
+
+if ! hash ec2-describe-regions 2>/dev/null; then
+  echo 'Error: Could not find "ec2-describe-regions" on PATH'
+  exit 1
+fi
 ec2-describe-regions -O $access_key -W $secret_key --region $region
 
 echo "Downloading machine image"
-wget http://tinycorelinux.net/6.x/x86_64/release/TinyCorePure64-6.4.1.iso
-export LOCAL_DISK_IMAGE_PATH=$PWD/TinyCorePure64-6.4.1.iso
+wget -O ${tmp_dir}/disk.iso http://tinycorelinux.net/6.x/x86_64/release/TinyCorePure64-6.4.1.iso
+export LOCAL_DISK_IMAGE_PATH=${tmp_dir}/disk.iso
 
 echo "Running integration tests"
 
-pushd builder-src > /dev/null
+pushd ${release_dir} > /dev/null
   . .envrc
   # TODO: re-enable errcheck (need to resolve errors found when `go get`ing)
   # go get github.com/kisielk/errcheck
