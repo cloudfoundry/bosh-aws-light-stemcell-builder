cd847ff464e3458b29b115651d89ed6cef197950
Caleb Miles <cmiles@pivotal.io>
2016-02-22 21:43:38 -0800

    Add debugging to status waiter.
    ...it always ways a little sketchy to match on strings.
    
    Signed-off-by: Edwin Xie <exie@pivotal.io>

--- src/light-stemcell-builder/ec2/status_waiter.go
+++ src/light-stemcell-builder/ec2/status_waiter.go
@@ -74,10 +74,12 @@ func WaitForStatus(status StatusFetcher, c WaiterConfig) (StatusInfo, error) {
 				retryCount++
 			}
 
+			fmt.Println("got status: %s, waiting for status: %s", info.Status(), c.DesiredStatus)
 			if info.Status() == c.DesiredStatus {
 				fmt.Println(fmt.Sprintf("%s matches desired status %s", c.Resource.ID(), c.DesiredStatus))
 				return info, nil
 			}
+
 		}
 	}
 }
