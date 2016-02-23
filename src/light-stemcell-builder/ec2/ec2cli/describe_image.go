29fc53eb5214f0d146756505655656b9b2a20c66
Corey Innis <cinnis@pivotal.io>
2016-02-23 11:37:05 -0800

    Split ec2 describe_image output on tabs instead of whitespace
    - Makes method more tolerant to empty/nonempty fields returns from
      ec2cli
    - Also adds table describing the fields returned ec2cli
    
    [#113412843](https://www.pivotaltracker.com/story/show/113412843)
    
    Signed-off-by: Lyle Franklin <lfranklin@pivotal.io>
    Signed-off-by: Corey Innis <cinnis@pivotal.io>

--- src/light-stemcell-builder/ec2/ec2cli/describe_image.go
+++ src/light-stemcell-builder/ec2/ec2cli/describe_image.go
@@ -42,8 +42,36 @@ func (e *EC2Cli) DescribeImage(amiResource ec2.StatusResource) (ec2.StatusInfo, 
 		return ec2ami.Info{}, ec2ami.NonAvailableAmiError{AmiID: amiConfig.AmiID, AmiStatus: ec2ami.AmiUnknownStatus}
 	}
 
-	firstLineFields := strings.Fields(outputLines[0])
-
+	firstLineFields := strings.Split(outputLines[0], "\t")
+	/*
+		Row 1 (IMAGE)
+			Column | Description
+			1 		 | The ID of the image
+			2 		 | The source of the image
+			3 		 | The date and time the image was created
+			4 		 | The status of the image
+			5 		 | The visibility of the image (public or private)
+			6 		 | The product codes, if any, that are attached to the instance
+			7 		 | The architecture of the image (i386 or x86_64)
+			8 		 | The image type (machine, kernel, or ramdisk)
+			9 		 | The ID of the kernel associated with the image (machine images only)
+			10		 | The ID of the RAM disk associated with the image (machine images only)
+			11		 | The platform of the image
+			12		 | The type of root device (ebs or instance-store)
+			13		 | The root device name
+			14		 | The virtualization type (paravirtual or hvm)
+			15		 | The Hypervisor type (xen or ovm)
+		Row 2 (BLOCKDEVICE)
+			1 		 | N/A
+			2 		 | The device name
+			3 		 | N/A
+			4 		 | The ID of the snapshot
+			5 		 | The volume size
+			6 		 | Indicates whether the volume is deleted on instance termination (true or false)
+			7 		 | The volume type
+			8 		 | N/A
+			9 		 | The encryption status of the volume
+	*/
 	imageInfo := ec2ami.Info{
 		AmiID:              amiConfig.AmiID,
 		Region:             amiConfig.Region,
@@ -51,16 +79,16 @@ func (e *EC2Cli) DescribeImage(amiResource ec2.StatusResource) (ec2.StatusInfo, 
 		Name:               firstLineFields[1],
 		ImageStatus:        firstLineFields[4],
 		Accessibility:      firstLineFields[5],
-		Architecture:       firstLineFields[6],
-		KernelId:           firstLineFields[8],
-		VirtualizationType: firstLineFields[10],
+		Architecture:       firstLineFields[7],
+		KernelId:           firstLineFields[9],
+		VirtualizationType: firstLineFields[14],
 	}
 
 	// If the block device mapping isn't set yet, then just return the first line fields
 	if len(outputLines) == 2 {
-		secondLineFields := strings.Fields(outputLines[1])
-		imageInfo.SnapshotID = secondLineFields[3]
-		imageInfo.StorageType = secondLineFields[6]
+		secondLineFields := strings.Split(outputLines[1], "\t")
+		imageInfo.SnapshotID = secondLineFields[4]
+		imageInfo.StorageType = secondLineFields[7]
 	}
 
 	return imageInfo, nil
