49f193e55b276267220a786556b3cd5b7894de3b
Edwin Xie <exie@pivotal.io>
2016-03-07 11:36:06 -0800

    Remove ec2-cli references from test script
    [#115074235](https://www.pivotaltracker.com/story/show/115074235)
    
    Signed-off-by: Lyle Franklin <lfranklin@pivotal.io>

--- ci/tasks/test.sh
+++ ci/tasks/test.sh
@@ -24,35 +24,18 @@ export AWS_SECRET_ACCESS_KEY=$secret_key
 
 export AWS_BUCKET_NAME=$bucket_name
 export AWS_REGION=$region
-export AMI_FIXTURE_ID=$ami_fixture_id
-
-export EBS_VOLUME_ID=${existing_volume_id}
-export EBS_SNAPSHOT_ID=${existing_snapshot_id}
-export S3_MACHINE_IMAGE_URL=${uploaded_machine_image_url}
-export MACHINE_IMAGE_PATH=${tmp_dir}/root.img
 export AWS_DESTINATION_REGION=${copy_region}
 
-export OUTPUT_STEMCELL_PATH=$PWD
+# Fixtures
+export S3_MACHINE_IMAGE_URL=${uploaded_machine_image_url}
+export EBS_VOLUME_ID=${existing_volume_id}
+export EBS_SNAPSHOT_ID=${existing_snapshot_id}
+export AMI_FIXTURE_ID=${ami_fixture_id}
 
-echo "Checking Java configuration"
-if hash java 2>/dev/null; then
-  JAVA_EXEC="$(which java)"
-else
-  JAVA_EXEC="$JAVA_HOME/bin/java"
-fi
-${JAVA_EXEC} -version
-
-echo "Checking EC2 CLI has been properly installed"
-
-if ! hash ec2-describe-regions 2>/dev/null; then
-  echo 'Error: Could not find "ec2-describe-regions" on PATH'
-  exit 1
-fi
-ec2-describe-regions -O $access_key -W $secret_key --region $region
+export MACHINE_IMAGE_PATH=${tmp_dir}/root.img
 
 echo "Downloading machine image"
-wget -O ${tmp_dir}/root.img ${uploaded_machine_image_url}
-export LOCAL_DISK_IMAGE_PATH=${tmp_dir}/root.img
+wget -O ${MACHINE_IMAGE_PATH} ${uploaded_machine_image_url}
 
 echo "Running all tests"
 
