b163b89341e7c2de708bd64dde539b10b981a6a2
Chris Nelson <cnelson@cnelson.org>
2017-06-29 19:13:20 -0700

    reenable debug sleep

--- src/light-stemcell-builder/driver/create_volume_driver.go
+++ src/light-stemcell-builder/driver/create_volume_driver.go
@@ -88,7 +88,7 @@ func (d *SDKCreateVolumeDriver) Create(driverConfig resources.VolumeDriverConfig
 	}
 
 	fmt.Println("=== SLEEPING FOR MANUAL DEBUGGING ===")
-	// time.Sleep(5 * time.Minute)
+	time.Sleep(5 * time.Minute)
 
 	reqOutput, err := d.ec2Client.ImportVolume(&ec2.ImportVolumeInput{
 		AvailabilityZone: availabilityZone,
