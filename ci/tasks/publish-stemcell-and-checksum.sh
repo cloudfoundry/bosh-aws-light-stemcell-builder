d027792e721de87a21c6352cb8e044066aa27d42
Zachary Gershman <zgershman@pivotal.io>
2017-11-30 15:21:43 -0800

    light stemcells now publish meta4 link
    - grabbing details from stemcells.MF file, wanted to make
    something that was not brittle
    
    [#152327196](https://www.pivotaltracker.com/story/show/152327196)
    
    Signed-off-by: Chris Dutra <cdutra@pivotal.io>

--- ci/tasks/publish-stemcell-and-checksum.sh
+++ ci/tasks/publish-stemcell-and-checksum.sh
@@ -14,6 +14,28 @@ light_stemcell_dir="$PWD/light-stemcell"
 light_stemcell_path="$(echo ${light_stemcell_dir}/*.tgz)"
 light_stemcell_name="$(basename "${light_stemcell_path}")"
 
+tar -Oxf ${light_stemcell_path} stemcell.MF > /tmp/stemcell.MF
+
+OS_NAME="$(bosh int /tmp/stemcell.MF --path /operating_system)"
+STEMCEL_VERSION="$(bosh int /tmp/stemcell.MF --path /version)"
+
+cp stemcells-index stemcells-index-output
+
+meta4_path=$PWD/stemcells-index-output/published/$OS_NAME/$STEMCELL_VERSION/stemcells.aws.meta4
+
+mkdir -p "$(dirname "${meta4_path}")"
+meta4 create --metalink="$meta4_path"
+
+meta4 import-file --metalink="$meta4_path" --version="$STEMCELL_VERSION" "light-stemcell/${light_stemcell_name}"
+meta4 file-set-url --metalink="$meta4_path" --file="${light_stemcell_name}" "https://s3.amazonaws.com/${OUTPUT_BUCKET}/${light_stemcell_name}"
+
+pushd stemcells-index-output > /dev/null
+  git add -A
+  git commit -c user.email="ci@localhost" \
+    -c user.name="CI Bot" \
+    -m "publish: $OS_NAME/$STEMCELL_VERSION"
+popd > /dev/null
+
 echo "Uploading light stemcell ${light_stemcell_name} to ${OUTPUT_BUCKET}..."
 aws s3 cp "${light_stemcell_path}" "s3://${OUTPUT_BUCKET}"
 
