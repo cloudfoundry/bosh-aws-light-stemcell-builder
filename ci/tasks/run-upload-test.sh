2826693b8c5a579df629c43bd8b2c06341e9797d
Rebecca Putinski <rputinski@pivotal.io>
2018-11-19 12:23:56 -0500

    [WIP] remove cn stemcell to save time for testing
    Co-authored-by: Behrouz Soroushian <bsoroushian@pivotal.io>

--- ci/tasks/run-upload-test.sh
+++ ci/tasks/run-upload-test.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 
-set -ex
+set -e
 
 source bosh-cpi-src/ci/utils.sh
 source director-state/director.env
 
+alias bosh='bosh-cli/bosh-*'
 
-pushd light-stemcell
+pushd stemcell
   time bosh -n upload-stemcell *.tgz
 popd
 
