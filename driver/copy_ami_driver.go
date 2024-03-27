8cb61f064e6b171cb8e2d1525cbf817a7cb03ab6
Kenneth Lakin <kenneth.lakin@broadcom.com>
2024-03-27 16:32:52 -0700

    Remove nolint check now that we are checking for error
    Signed-off-by: Joseph Palermo <joseph.palermo@broadcom.com>
    Co-authored-by: Joseph Palermo <joseph.palermo@broadcom.com>

--- driver/copy_ami_driver.go
+++ driver/copy_ami_driver.go
@@ -128,7 +128,7 @@ func (d *SDKCopyAmiDriver) Create(driverConfig resources.AmiDriverConfig) (resou
 
 	if driverConfig.Accessibility == resources.PublicAmiAccessibility {
 		d.logger.Printf("making AMI: %s public", *amiIDptr)
-		_, err = ec2Client.ModifyImageAttribute(&ec2.ModifyImageAttributeInput{ //nolint:errcheck
+		_, err = ec2Client.ModifyImageAttribute(&ec2.ModifyImageAttributeInput{
 			ImageId: amiIDptr,
 			LaunchPermission: &ec2.LaunchPermissionModifications{
 				Add: []*ec2.LaunchPermission{
